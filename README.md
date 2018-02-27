434cce01ea09e8621dbf6c97c29d886bad1f07d6
Suzanne Richman <suzanne@favoredfortune.com>
2018-02-27 11:44:54 -0800

    created and pushed database to Heroku for model and updated server.js with correct connections

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 **Authors**: Rebecca Prows and Suzanne Richman 
 
-**Version**: 1.0.0 
+**Version**: 1.0.1
 
 ## Overview
 
@@ -11,11 +11,16 @@
 
 
 ## Architecture
-<!-- Provide a detailed description of the application design. What technologies (languages, libraries, etc) you're using, and any other relevant design information. -->
+
 
 ## Change Log
 2/26/2018 
 4:00pm- Set up basic file structure
+2/27/2018
+9:40 - add eslintrc file
+11:45 - created and pushed database to Heroku for model and updated server.js with correct connections
 
 ## Credits and Collaborations
-Thanks to our Instructers and TAs! 
+Thanks to our Instructers and TAs!
+
+For PostgreSQL info: https://www.postgresql.org/docs/10/static/datatype-character.html
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const pg = require('pg');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -16,7 +16,8 @@ app.get('/',(request, response) => response.send('Testing 1, 2, 3'));
 
 app.listen(PORT, ()=> console.log(`listening on PORT:${PORT}`));
 
+
 // PORT=3000
-// CLIENT_URL = http://localhost:8000
-// DATABASE_URL=postgres://localhost:5432/booklist
+// CLIENT_URL = http://localhost:3000
+// DATABASE_URL= postgres://monlhvsyldctvl:348229b16f7b352c54f13c2411a9df98c9d2b16f8f8cc9a9720e1076a64fa3de@ec2-174-129-26-203.compute-1.amazonaws.com:5432/df40pmm7k2pt0q
 
